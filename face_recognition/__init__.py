767c69b6a70e981b11cc8c78642495b5886b87fa
unknown <10000347@yibandc.com>
2018-01-05 11:48:17 +0800

    move opencv in face_recognition

---
+++ face_recognition/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: v1.0
+@author: duanwei
+@license: Apache Licence 
+@contact: 4064865@qq.com
+@site: http://blog.csdn.net/dwshmilyss
+@software: PyCharm
+@file: __init__.py.py
+@time: 2018/1/5 11:41
+"""
+import sys
+
+default_encoding = "utf-8"
+if (default_encoding != sys.getdefaultencoding()):
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
